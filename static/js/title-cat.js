de89264e27269166ffdc14f88d5a3f7ccf5a2753
Rohan Menon <rmenon1008@gmail.com>
2025-10-26 10:45:15 -0400

    Update cats

--- static/js/title-cat.js
+++ static/js/title-cat.js
@@ -6,65 +6,88 @@ setTimeout(() => {
     const catWalkLength = document.getElementById("inbetween-title-canvas").width;
     console.log(catWalkLength);
 
-    const spriteSheet = new createjs.SpriteSheet({
+    const blueSheet = new createjs.SpriteSheet({
         framerate: 8,
-        images: ["/media/cat_walking.png"],
+        images: ["/media/blue_walking.png"],
         frames: { regX: 0, regY: 0, width: 18, height: 16, count: 25 },
         animations: {
-            walk: [0, 7, "walk", 8 / 8],
-            walkToSit: [8, 9, "sit", 8 / 8],
-            sit: [10, 17, "sit", 5 / 8],
+            walk: [0, 7, "walk", 6 / 8],
+            walkToSit: [8, 9, "sit", 7 / 8],
+            sit: [10, 17, "sit", 4 / 8],
             nuzzleEnter: [18, 18, "nuzzle", 6 / 8],
-            nuzzle: [19, 22, "nuzzle", 6 / 8],
+            nuzzle: [19, 22, "nuzzle", 8 / 8],
             nuzzleExit: [23, 24, "sit", 6 / 8],
         },
     });
 
-    const cat = new createjs.Sprite(spriteSheet, "walk");
-    cat.gotoAndPlay("walk");
-    cat.x = catWalkLength;
-    cat.addEventListener("mousedown", nuzzleEnter);
-    cat.addEventListener("pressup", nuzzleExit);
-    stage.addChild(cat);
+    const lucySheet = new createjs.SpriteSheet({
+        framerate: 8,
+        images: ["/media/lucy_walking.png"],
+        frames: { regX: 0, regY: 0, width: 18, height: 16, count: 25 },
+        animations: {
+            walk: [0, 7, "walk", 8 / 8],
+            walkToSit: [8, 9, "sit", 8 / 8],
+            sit: [10, 17, "sit", 7 / 8],
+            nuzzleEnter: [18, 18, "nuzzle", 6 / 8],
+            nuzzle: [19, 22, "nuzzle", 8 / 8],
+            nuzzleExit: [23, 24, "sit", 6 / 8],
+        },
+    });
 
+    const blue = new createjs.Sprite(blueSheet, "walk");
+    const lucy = new createjs.Sprite(lucySheet, "walk");
+    blue.gotoAndPlay("walk");
+    lucy.gotoAndPlay("walk");
+    blue.x = catWalkLength;
+    lucy.x = catWalkLength + 70;
+    blue.addEventListener("mousedown", nuzzleEnterBlue);
+    blue.addEventListener("pressup", nuzzleExitBlue);
+    lucy.addEventListener("mousedown", nuzzleEnterLucy);
+    lucy.addEventListener("pressup", nuzzleExitLucy);
+    stage.addChild(blue);
+    stage.addChild(lucy);
     createjs.Ticker.framerate = 8;
     createjs.Ticker.addEventListener("tick", tick);
 
     function tick(event) {
-        if (cat.currentAnimation === "walk") {
-            cat.x -= 2;
+        if (blue.currentAnimation === "walk") {
+            blue.x -= 2;
 
-            if (cat.x <= 0) {
-                cat.gotoAndPlay("walkToSit");
+            if (blue.x <= 0) {
+                blue.gotoAndPlay("walkToSit");
+            }
+        }
+        if (lucy.currentAnimation === "walk") {
+            lucy.x -= 3;
+
+            if (lucy.x <= 10) {
+                lucy.gotoAndPlay("walkToSit");
             }
         }
         stage.update(event);
     }
 
-    let start;
-    let end;
-
-    function nuzzleEnter() {
-        if (cat.currentAnimation === "sit") {
-            cat.gotoAndPlay("nuzzleEnter");
-            start = new Date().getTime();
+    function nuzzleEnterBlue() {
+        if (blue.currentAnimation === "sit") {
+            blue.gotoAndPlay("nuzzleEnter");
         }
     }
 
-    function nuzzleExit() {
-        if (cat.currentAnimation === "nuzzle" || cat.currentAnimation === "nuzzleEnter") {
-            cat.gotoAndPlay("nuzzleExit");
-            end = new Date().getTime();
-            const petTime = end - start;
+    function nuzzleEnterLucy() {
+        if (lucy.currentAnimation === "sit") {
+            lucy.gotoAndPlay("nuzzleEnter");
+        }
+    }
 
-            // Make a POST request to the server to update the pet time
-            fetch("https://pet-tracking.vercel.app/log_pet", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ duration_ms: petTime }),
-            });
+    function nuzzleExitBlue() {
+        if (blue.currentAnimation === "nuzzle" || blue.currentAnimation === "nuzzleEnter") {
+            blue.gotoAndPlay("nuzzleExit");
+        }
+    }
+
+    function nuzzleExitLucy() {
+        if (lucy.currentAnimation === "nuzzle" || lucy.currentAnimation === "nuzzleEnter") {
+            lucy.gotoAndPlay("nuzzleExit");
         }
     }
 }, 100);
